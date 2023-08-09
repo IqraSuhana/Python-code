e76b016c3a7f7dee83c528e89ab6f6dbebdc49be
Iqra <122859001+IqraSuhana@users.noreply.github.com>
2023-08-09 08:07:31 +0500

    Add files via upload

---
+++ 1_tuple.py
@@ -0,0 +1,13 @@
+tup_1=(1,2,3,"iqra",2);
+tup_2=6,"irfan",98
+print(tup_2)
+# print(tup_1.count("iqra")); #count the repetetiveness of particular element
+# print(tup_1.index("iqra"))
+tup_2=tup_1;
+print(tup_2)
+
+# del(tup_2[2]); not supported
+# tup_2[0]=7;    not allowed
+# print(tup_1.len);   not supported
+print(len(tup_1));
+print()
