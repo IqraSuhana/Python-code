22787208241483bb5e4a69f21ddbcf6a5ed2aa80
Iqra <122859001+IqraSuhana@users.noreply.github.com>
2023-08-09 08:09:07 +0500

    Add files via upload

---
+++ 3_dictionary.py
@@ -0,0 +1,8 @@
+#dictionary in python is just like object in javascript, akey value pair
+#if the key is string,it must be enclosed in quotes
+dict={
+    "name":"iqra",
+    "age":18
+}
+#accessing
+print(dict["name"])
