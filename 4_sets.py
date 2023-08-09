e752cbcf9ffe80d8c75469063e6ada95f92a8f0c
Iqra <122859001+IqraSuhana@users.noreply.github.com>
2023-08-09 08:09:33 +0500

    Add files via upload

---
+++ 4_sets.py
@@ -0,0 +1,8 @@
+dic={1:{"name":"iqra","field":"engineering"},2:{"name":"irfan","field":"medical"}}
+print(dic[1]["name"])
+for select in range(len(dic)):
+    print(1);
+
+def sum(a):
+    return a, 1
+print(sum(2))
