03be7c8b40e41e073fece9f662a43ef691fd3c04
Iqra <122859001+IqraSuhana@users.noreply.github.com>
2023-08-11 08:44:46 +0500

    Add files via upload

---
+++ Creating a package/MyPackage/__init__.py
@@ -0,0 +1,13 @@
+from . import module1
+from . import module2
+
+#now verify the package by
+# 1- opening the bash terminal
+# 2- change the directory to folder where your package is located
+# 3- open the python interpreter by running the command "python"
+# 4- using the python prompt, type 'import project_name'
+# 5- if this command is run successfully without error, package is successfully loaded
+ 
+# General structure for testing package is 
+#   {package_name}.{module_name}.{function_name}(parameters)
+# without curly brackets
